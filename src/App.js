31ddbf452288ad07d2a7ce276b4b43798d202a2d
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-09-29 02:36:08 +0600

    homepage template set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='course-container'>
+        <div className='course-holder'>
+          <div className='container'>
+            <h2>Course Name</h2>
+          </div>
+        </div>
+        <div className='profile-holder'>
+          <div className='container'>
+            <h2>profile</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
